4e56fe735f99226fc6470f51c28c3ca0a4d144e2
Brett Peat <brett.peat@capitalone.com>
2017-07-19 15:47:22 -0400

    continued game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,20 @@
 var cards = ["Queen" , "Queen" , "King" , "King"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ("User flipped " + cardsInPlay[0]);
-var cardTwo = cards[1];
-cardsInPlay.push (cardTwo);
-console.log ("User flipped " + cardsInPlay[1]);
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]){
-    alert ("Congratulations! You found a match!")
-  }
-  else {
-    alert ("Sorry. Please try again! You can do it!")
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]){
+      alert ("Congratulations! You found a match!")
+    }
+    else {
+      alert ("Sorry. Please try again! You can do it!")
+    }
   }
 }
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId])
+  cardsInPlay.push (cards[cardId])
+  checkForMatch()
+}
+
+flipCard(0)
+flipCard(2)
