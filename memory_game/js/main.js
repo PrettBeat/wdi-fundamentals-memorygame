98f76d7bcc973375eb6d5fd12cc883ec5e7259c6
Brett Peat <brett.peat@capitalone.com>
2017-07-19 10:59:50 -0400

    Created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log ("Up and running!");
+var cardOne = 'Queen';
+var cardTwo = 'King';
+var cardThree = 'Queen';
+var cardFour = 'King';
+console.log ("The user picked " + cardOne);
+console.log ("The user picked " + cardTwo);
