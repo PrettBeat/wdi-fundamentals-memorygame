af8c33c54b4de5080bdd5db6db8f83756e5b6311
Brett Peat <brett.peat@capitalone.com>
2017-07-20 11:51:34 -0400

    added objects to main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,46 @@
-var cards = ["Queen" , "Queen" , "King" , "King"];
+var cards = [
+  {
+    rank: "Queen",
+    suit: "Hearts",
+    image: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "Queen",
+    suit: "diamonds",
+    image: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "King",
+    suit: "Hearts",
+    image: "images/king-of-hearts.png"
+  },
+  {
+    rank: "King",
+    suit: "Diamonds",
+    image: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]){
-      alert ("Congratulations! You found a match!")
+      alert ("Congratulations! You found a match!");
     }
     else {
-      alert ("Sorry. Please try again! You can do it!")
+      alert ("Sorry. Please try again! You can do it!");
     }
   }
 }
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId])
-  cardsInPlay.push (cards[cardId])
-  checkForMatch()
+  console.log("User flipped " + cards[cardId].rank);
+  cardsInPlay.push (cards[cardId].rank);
+  checkForMatch();
 }
 
-flipCard(0)
-flipCard(2)
+flipCard(0);
+flipCard(2);
+
+console.log (cards[0].image);
+console.log (cards[0].suit);
+console.log (cards[2].image);
+console.log (cards[2].suit);
